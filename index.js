cfaf2ff57204311c254a2f21553454a6f59a1ad7
haiDbarbosa <haianlobarbosa@gmail.com>
2024-01-19 21:24:55 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+let victories = calculateVictory(64,12)
+
+function calculateVictory(victories, defeat) {
+    return victories - defeat;
+}
+
+// function totalMatchs() {
+    
+// }
+
+console.log(`Você tem o total de partidas! O saldo de vitórias é de ${victoryRoyal()}`)
